52803b9b106d8daee7e894ebd2bbf6e0a2d28896
rcs333 <ryanshean1@gmail.com>
2017-02-16 18:18:21 -0800

    Fixed all the bugs that i've run into so far - handles annotations for ribosomal slippage AND rna editing by HPIV/Measels/Mumps for right now - with some basic code for Nipah and sendai as well just in case i wanna start adding those

--- sorter.py
+++ sorter.py
@@ -1,5 +1,7 @@
-# We gon make a small version of our blaster and sorter here that just takes a fasta, cause this is as far back as
-# I really want to go for right now
+# Automatically annotates fastas of well known viruses based off maaft alignment to best blast hits
+# currently all you need to do is set your blastdb location, make a blank metadata sheet, and set an input fasta
+# coverage is automatically generated if we don't have information on our sheet so be aware of that
+# Can currently handle - Coronavirus ribosomal slippage, HPIV/Measels/Mumps RNA editing
 
 import subprocess
 import re
@@ -7,36 +9,13 @@ from random import randint
 import argparse
 import timeit
 import os
+from Bio.Seq import Seq
 
-# TODO: Clean up this whole top section
-# Top line is ribosomal slippage
-# bottom line is for rna editing, in case I want to handle these guys differently
-
-#hard_list = ['human coronavirus 229E', 'Human coronavirus OC43', 'Human parainfluenza virus 3', 'astrovirus 1', 'west nile', 'HIV', 'HTLV', 'influenza A',
-#             'HPIV', 'measels', 'mumps', 'ebola', 'Human parainfluenza virus 4', 'Human parainfluenza virus 2']
-#non_specific_hard_list = ['coronavirus', 't-lymphotropic', 'immunodeficiency', 'influenza', 'measels', 'mumps', 'nipah', 'ebola']
-
-# Set up the environment variablesd
-fasta_file_loc = 'SC261.fasta'
 BLAST_DB_LOCATION = '/Users/uwvirongs/Downloads/surpi-master/nt'
 
-# grab the strain name so we can actually begin to generalize this
-#TODO: when we up this to accept fasta with a lot of inputs we'll save the fasta inside the folder as well
-#for line in open(fasta_file_loc):
-#    if line[0] == '>':
-#        strain = line[1:-1]
 
-# make us a directory
-#subprocess.call('mkdir -p ' + strain, shell=True)
-
-# TODO: only commented out to save time each step
-# blast us this fasta and save the top 25 hits into a file
-#subprocess.call('blastn -query ' + fasta_file_loc + ' -db ' + BLAST_DB_LOCATION + ' -num_threads 8 -num_descriptions 0 '
-#                '-num_alignments 25 -word_size 28 | tee ' + strain + '/' + strain + '.blastresults', shell=True)
-
-# read through the top 25 hits and save the top hit that has the word complete in it - if we don't do this fail miserably
-
-
+# Reads in a fasta file that should have strain names for the names of the sequences -  can handle any number
+# Returns a list with the names of the strains and the strings of the genomes
 def read_fasta(fasta_file_loc):
     strain_list = []
     genome_list = []
@@ -53,17 +32,22 @@ def read_fasta(fasta_file_loc):
     return strain_list, genome_list
 
 
+# This function takes the strain name and a location of the individual fasta file we saved earlier and runs a blast
+# Search saving the top 35 hits - then the top hit is found that is a complete genome and the fasta and .gbk of that
+# are saved - then we run a mafft alignment on the two and return two strings one of them our sequence with '-' and the
+# other of our new reference sequence
+# TODO: probobly split this into multiple functions or at least rename this one
 def blast_n_shit(strain, our_fasta_loc):
-    # If we've already done this one before skip the blasting step, should speed up error chekcing in the futuroe
+    # If we've already done this one before skip the blasting step, should speed up error checking in the future
     if not os.path.isfile(strain + '/' + strain +'.blastresults'):
-        subprocess.call('blastn -query ' + our_fasta_loc + ' -db ' + BLAST_DB_LOCATION + ' -num_threads 8 -num_descriptions 0 '
-                        '-num_alignments 25 -word_size 28 | tee ' + strain + '/' + strain + '.blastresults', shell=True)
+        subprocess.call('blastn -query ' + our_fasta_loc + ' -db ' + BLAST_DB_LOCATION + ' -num_threads 8 '
+                        '-num_descriptions 0 -num_alignments 35 -word_size 28 | tee ' + strain + '/' + strain +
+                        '.blastresults', shell=True)
 
     # read through the top 25 hits saved earlier and save the acsession number of the best hit that's complete
     read_next = False
     for line in open(strain + '/' + strain + '.blastresults'):
-        # This line removed because blast is mostly screwy and I'm pretty sure we're garunteed complete genomes here
-        if line[0] == '>': # and 'complete' in line:
+        if line[0] == '>':
             name_of_virus = line.split('|')[4].split('strain')[0].split('isolate')[0].strip()
             ref_seq_gb = line.split('|')[3]
             if 'complete' in line:
@@ -81,27 +65,34 @@ def blast_n_shit(strain, our_fasta_loc):
         ref_seq_gb = 'KY369913.1'
 
     #  here we take the blast results and save both the fasta and the gbk file for pulling of annotations
-    cmd = '/Users/uwvirongs/edirect/esearch -db nucleotide -query ' + ref_seq_gb + ' | /Users/uwvirongs/edirect/efetch -format fasta > ' + strain + '/' + strain + '_ref.fasta'
+    cmd = '/Users/uwvirongs/edirect/esearch -db nucleotide -query ' + ref_seq_gb + \
+          ' | /Users/uwvirongs/edirect/efetch -format fasta > ' + strain + '/' + strain + '_ref.fasta'
     ps = subprocess.Popen(cmd, stdin=subprocess.PIPE, shell=True)
     ps.communicate()
 
-    cmd = '/Users/uwvirongs/edirect/esearch -db nucleotide -query ' + ref_seq_gb + ' | /Users/uwvirongs/edirect/efetch -format gb > ' + strain + '/' + strain + '_ref.gbk'
+    cmd = '/Users/uwvirongs/edirect/esearch -db nucleotide -query ' + ref_seq_gb + \
+          ' | /Users/uwvirongs/edirect/efetch -format gb > ' + strain + '/' + strain + '_ref.gbk'
 
     ds = subprocess.Popen(cmd, stdin=subprocess.PIPE, shell=True)
     ds.communicate()
 
-    subprocess.call('cat ' + strain + '/' + strain + '_ref.fasta ' + strain + '/' + strain + '.fasta > ' + strain + '/' + strain + '.aligner', shell=True)
-    subprocess.call('mafft --auto ' + strain + '/' + strain + '.aligner > ' + strain + '/' + strain + '.ali', shell=True)
+    subprocess.call('cat ' + strain + '/' + strain + '_ref.fasta ' + strain + '/' + strain + '.fasta > ' + strain +
+                    '/' + strain + '.aligner', shell=True)
+    subprocess.call('mafft --auto ' + strain + '/' + strain + '.aligner > ' + strain + '/' + strain + '.ali',
+                    shell=True)
     # returns name of virus - and has saved .ali file as well as a .gbk file
 
     # simply splits the aligned data into two different strings
     ali_list, ali_genomes = read_fasta(strain + '/' + strain + '.ali')
     ref_seq = ali_genomes[0]
     our_seq = ali_genomes[1]
+    # The two strings returned have gap information in the form '-' use "genome" variable for the actual viral genome
     return name_of_virus, our_seq, ref_seq
 
 
 # Takes the name of one of the virus samples and returns a list of all the metadata we have in the sheet
+# Currently this is set up horribly and actually might result in coverages being words or something - when I update
+# The sheet I'll need to change these hardcoded locations to ones that work
 def pull_metadata(virus_name, meta_data_info_sheet_loc):
     for line in open(meta_data_info_sheet_loc):
         if line.split(',')[1] == virus_name:
@@ -131,6 +122,9 @@ def find_loc_in_our_seq_from_reference(start, our_seq, reference_seq):
     return our_index
 
 
+# Takes in two sequences with gaps inserted inside of them and returns arrays that have a -1 in the gap locations and
+# count up from 1 in the nucleotide areas - This data structure allows for extremely rapid conversion between relative
+# locations in the two sequences
 def build_num_arrays(our_seq, ref_seq):
     ref_count = 0
     our_count = 0
@@ -153,14 +147,16 @@ def build_num_arrays(our_seq, ref_seq):
     return our_num_array, ref_num_array
 
 
+# Takes a gene start index relative to an unaligned reference sequence and then returns the location of the same start
+# area on the unaligned sequence that we're annotating using the number arrays to finish
 def adjust(given_num, our_num_array, ref_num_array):
 
     # Go through our number array and search for the number of interest
     for x in range(0, len(our_num_array)):
         if ref_num_array[x] == given_num:
-            print('what should be equal to ' + str(given_num) + ' is ' + str(ref_num_array[x]))
-            print('and this is the index ' + str(x))
-            print('and array at position ' + str(x) + ' is ' + str(our_num_array[x]))
+            #print('what should be equal to ' + str(given_num) + ' is ' + str(ref_num_array[x]))
+            #print('and this is the index ' + str(x))
+            #print('and array at position ' + str(x) + ' is ' + str(our_num_array[x]))
             index = x
             break
 
@@ -168,36 +164,33 @@ def adjust(given_num, our_num_array, ref_num_array):
     return str(our_num_array[index])
 
 
+# this opens up the reference .gbk file and pulls all of the annotations, it then adjusts the annotations to the
+# relative locations that they should appear on our sequence
 def pull_correct_annotations(strain, our_seq, ref_seq):
     # Read the reference gff file and extract lists of all of the protein locations and annotations!
     gene_loc_list = []
     gene_product_list = []
-    print(our_seq)
-    print(ref_seq)
+
     for line in open(strain + '/' + strain + '_ref.gbk'):
         if ' CDS ' in line:
-            #print(line)
             # this is now going to be a list of numbers, start-stop start-stop
             gene_loc_list.append(re.findall(r'\d+', line))
             allow_one = True
         if '/product="' in line and allow_one:
             allow_one = False
-            #print(line)
             gene_product_list.append(line.split('=')[1][1:-2])
-    #print('gene locs before adjustment')
-    #print(gene_loc_list)
+
     our_seq_num_array, ref_seq_num_array = build_num_arrays(our_seq, ref_seq)
-    print(our_seq_num_array)
-    print(ref_seq_num_array)
+
+    # Adjust every locus so that we actually put in correct annotations
     for x in range(0, len(gene_loc_list)):
         for y in range(0, len(gene_loc_list[x])):
             gene_loc_list[x][y] = adjust(int(gene_loc_list[x][y]), our_seq_num_array, ref_seq_num_array)
-    #print('products and numbers after adjustment')
-    #print(gene_product_list)
-    #print(gene_loc_list)
+
     return gene_loc_list, gene_product_list
 
 
+# takes a strain name and a genome and writes and saves a fasta to the correct directory
 def write_fasta(strain, genome):
     w = open(strain + '/' + strain + '.fasta', 'w')
     w.write('>' + strain + '\n')
@@ -228,18 +221,20 @@ def write_cmt(sample_name, coverage):
     cmt.close()
 
 
-def write_tbl(strain, gene_product_list, gene_locations, genome):
-    #print('now we in write_tbl')
-    #print('gene products')
-    #print(gene_product_list)
-    #print('gene locations')
-    #print(gene_locations)
+# this takes in all of our information and makes a feature table that actually should work
+# annotations for ribosomal slippage and RNA editing should be working now - as well as creation of a .pep file for rna
+# editing
+def write_tbl(strain, gene_product_list, gene_locations, genome, gene_of_intrest, note):
+
     tbl = open(strain + '/' + strain + '.tbl', 'w')
     tbl.write('>Feature ' + strain)
     flag = ''
+    xtra = ''
     # TODO: add the ribosomal slippage line here
     for x in range(0, len(gene_product_list)):
         product = gene_product_list[x]
+        if gene_of_intrest in product:
+            xtra = note
         location_info = gene_locations[x]
         if len(location_info) == 4:
             start_1 = str(location_info[0])
@@ -259,12 +254,12 @@ def write_tbl(strain, gene_product_list, gene_locations, genome):
                 else:
                     flag = '>'
             tbl.write('\n' + str(start) + '\t' + flag + str(end) + '\tCDS\n')
-            tbl.write('\t\t\tproduct\t' + product)
+            tbl.write('\t\t\tproduct\t' + product + xtra)
+        xtra = ''
     tbl.close()
 
-def rna_editing():
-    x =0
 
+# takes a single strain name and a single genome and annotates and save the entire virus and annotations package
 def annotate_a_virus(strain, genome, metadata_location, sbt_loc):
     subprocess.call('mkdir -p ' + strain, shell=True)
 
@@ -284,21 +279,69 @@ def annotate_a_virus(strain, genome, metadata_location, sbt_loc):
 
     write_fsa(strain, name_of_virus, genome)
 
-    write_tbl(strain, gene_product_list, gene_loc_list, our_seq)
-
+    #TODO: this should all be moved into a seperate function
+    extra_stuff = ''
+    # No fucking protein had better be named this
+    gene_of_interest = 'XFNDKLS:NLFKSD:FJNSDLKFJDSLKFJDLFUHE:OPUHFE:LUHILDLKFJNSDLFKJBNDLKFUHSLDUBFKNLKDFJBLSKDJFBLDKS'
     if 'parainfluenza virus' in name_of_virus.lower():
         if '3' in name_of_virus:
-            x = 0
-            # go into phosphoprotein - find poly G track and 2 g's and +2 to stop codon and then manually translate it
-            # Save translation so that we can overwrite the .sqn file
-        elif '4b' in name_of_virus:
-            x = 0
-            # Same as 3 but do it with the D protein instead
+            extra_stuff = '\n\t\t\texception\tRNA Editing\n\t\t\tnote\tRNA Polymerase adds non templeted ' \
+                          'Gs\n\t\t\tprotein_id\t69'
+            gene_of_interest = 'D protein'
+            process_para(strain, genome, gene_loc_list, gene_product_list, 'D protein', 'HP3')
+        elif '4' or '1' in name_of_virus:
+            extra_stuff = '\n\t\t\texception\tRNA Editing\n\t\t\tnote\tRNA Polymerase adds 2 non templeted ' \
+                          'Gs\n\t\t\tprotein_id\t69'
+            gene_of_interest = 'phosphoprotein'
+            process_para(strain, genome, gene_loc_list, gene_product_list, 'phosphoprotein', 'HP4-1')
+    # Sorta adding more - although I think this should definitely be handled elsewhere
+    if 'measles' in name_of_virus.lower():
+        extra_stuff = '\n\t\t\texception\tRNA Editing\n\t\t\tnote\tRNA Polymerase adds 1 non templeted ' \
+                      'G\n\t\t\tprotein_id\t69'
+        gene_of_interest = 'V protein'
+        process_para(strain, genome, gene_loc_list, gene_product_list, 'V protein', 'MEAS')
+    if 'mumps' in name_of_virus.lower():
+        extra_stuff = '\n\t\t\texception\tRNA Editing\n\t\t\tnote\tRNA Polymerase adds 2 non templeted ' \
+                      'G\n\t\t\tprotein_id\t69'
+        gene_of_interest = 'phosphoprotein'
+        process_para(strain, genome, gene_loc_list, gene_product_list, gene_of_interest, 'MUMP')
+
+    write_tbl(strain, gene_product_list, gene_loc_list, genome, gene_of_interest, extra_stuff)
 
     subprocess.call('tbl2asn -p ' + strain + '/ -t ' + strain + '/' + sbt_loc.split('/')[-1] +
                     ' -Y ' + strain + '/assembly.cmt -V vb', shell=True)
 
 
+# Takes a virus that has GGGGGG RNA editing and based on the gene that you send it and the name of the virus will
+# find that gene in our annotations - add the correct number of G's and then translate the new 'mRNA' and write the
+# translation to a .pep file where we can overwrite the sequin auto-translation
+def process_para(strain, genome, gene_loc_list, gene_product_list, gene_of_interest, v):
+    # Extract the gene
+    for x in range(0, len(gene_product_list)):
+        if gene_of_interest in gene_product_list[x]:
+            nts_of_gene = genome[int(gene_loc_list[x][0]) - 1:int(gene_loc_list[x][1]) - 1]
+            break
+    start_of_poly_g = nts_of_gene.find('GGGGG')
+
+    # add the correct number of Gs
+    if v == 'HP3':
+        nts_of_gene = nts_of_gene[0:start_of_poly_g + 1] + 'G' + nts_of_gene[start_of_poly_g + 1:]
+        while len(nts_of_gene) % 3 != 0:
+            nts_of_gene = nts_of_gene[0:start_of_poly_g + 1] + 'G' + nts_of_gene[start_of_poly_g+1:]
+    elif v == 'HP4-1' or v == 'MUMP':
+        nts_of_gene = nts_of_gene[0:start_of_poly_g + 1] + 'GG' + nts_of_gene[start_of_poly_g + 1:]
+    elif v == 'MEAS' or v == 'SENDAI' or v == 'NIPAH':
+        nts_of_gene = nts_of_gene[0:start_of_poly_g + 1] + 'G' + nts_of_gene[start_of_poly_g + 1:]
+
+    new_translation = str(Seq(nts_of_gene).translate())
+
+    pep = open(strain + '/' + strain + '.pep', 'w')
+    pep.write('>69\n' + new_translation)
+    pep.close()
+
+
+# Writes an fsa file based of the name, strain and genome, honestly we should allow for much more flexibility
+# and automation here
 def write_fsa(strain, name_of_virus, virus_genome):
     fsa = open(strain + '/' + strain + '.fsa', 'w')
     fsa.write('>' + strain + ' [organism=' + name_of_virus + '] [collection-date=2016] [country=USA] '
@@ -307,9 +350,21 @@ def write_fsa(strain, name_of_virus, virus_genome):
     fsa.close()
 
 
+# Takes the name of a recently created .gbf file and checks it for stop codons (which usually indicate something went
+# wrong. NOTE: requires tbl2asn to have successfully created a .gbf file or this will fail catastrophically
+def check_for_stops(sample_name):
+    stops = 0
+    for line in open(sample_name + '/' + sample_name + '.gbf'):
+        if '*' in line:
+            stops += 1
+    if stops > 0:
+        print('WARNING: ' + sample_name + ' contains ' + str(stops) + 'stop codon(s)!')
+
 if __name__ == '__main__':
+
+    # Set this to where you want your
     fasta_loc = 'HarvestSeqJ1-J2-J4-J5.fasta'
-    start = timeit.default_timer()
+    start_time = timeit.default_timer()
 
     # TODO: add a flag for redoing tbl2asn that ONLY does that - i.e. you could manually edit files then crank em out
 #    parser = argparse.ArgumentParser(description='Package a set of UW clinical virus sequences for submission, pulling '
@@ -324,10 +379,10 @@ if __name__ == '__main__':
 
     virus_strain_list, virus_genome_list = read_fasta(fasta_loc)
 
-    # TODO: change the name of the arguments
-    # TODO: go look at my virus_annotate_code for how to read in fastas
-    # TODO: then go ahead and change this to a looping generalizable program that'll actually function
-    # TODO: test
     for x in range(0, len(virus_strain_list)):
         annotate_a_virus(virus_strain_list[x], virus_genome_list[x], 'UWVIROCLINSEQ.csv', 'template.sbt')
 
+    for name in virus_strain_list:
+        check_for_stops(name)
+
+    print('WOOO! only took ' + str(timeit.default_timer() - start_time))
