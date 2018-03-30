7b8fcde723f08fac7880536d3ee91ab5fc871ba4
rcs333 <ryanshean1@gmail.com>
2018-03-30 14:36:55 -0700

    test

--- vapid.py
+++ vapid.py
@@ -107,6 +107,7 @@ def blast_n_stuff(strain, our_fasta_loc):
         ref_seq_gb = args.r
     if 'CORONAVIRUS 229E' in name_of_virus.upper():
         ref_seq_gb = 'KY369913.1'
+        # this code might need to be tweaked a bit, but for now it's working
     #if 'HUMAN RESPIROVIRUS 3' in name_of_virus.upper():
     #    ref_seq_gb = 'KY369864'
     if 'HUMAN IMMUNODEFICIENCY VIRUS TYPE 1' in name_of_virus.upper():
