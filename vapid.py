8ca6bf8612754c945a81d3b233115d5ba01a30c4
rcs333 <ryanshean1@gmail.com>
2018-04-11 19:38:38 -0700

    test

--- vapid.py
+++ vapid.py
@@ -191,6 +191,8 @@ def build_num_arrays(our_seq, ref_seq):
 # Takes a gene start index relative to an unaligned reference sequence and then returns the location of the same start
 # area on the unaligned sequence that we're annotating using the number arrays to finish
 def adjust(given_num, our_num_array, ref_num_array):
+    print("HERES OUR GIVEN NUM THAT WE CANT FIND")
+    print(given_num)
     print(ref_num_array)
     # Go through our number array and search for the number of interest
     for x in range(0, len(our_num_array)):
