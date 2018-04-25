3402c96b0031fe9178ae6a68f3aaab887d24b8bb
rcs333 <ryanshean1@gmail.com>
2018-04-24 18:19:06 -0700

    test

--- vapid.py
+++ vapid.py
@@ -354,6 +354,7 @@ def write_tbl(strain, gene_product_list, gene_locations, genome, gene_of_intrest
             if int(start) < 1:
                 sflag = '<'
                 xtra = '\t\t\tcodon_start\t' + str((int(end) % 3) + 1)
+                start = '1'
 
             if int(end) < 1 or int(end) < int(start):
                 end = len(genome)
