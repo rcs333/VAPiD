17d03835d3292ea19f3c466c82d998a77ac6fbdb
rcs333 <ryanshean1@gmail.com>
2018-04-25 14:56:54 -0700

    test

--- vapid.py
+++ vapid.py
@@ -299,7 +299,7 @@ def write_tbl(strain, gene_product_list, gene_locations, genome, gene_of_intrest
     tbl.write('>Feature ' + strain)
 
     for x in range(0, len(gene_product_list)):
-        print(gene_product_list[x] + ' ' + str(gene_locations[x][0]))
+        print(gene_product_list[x] + ' ' + str(gene_locations[x]))
         flag = ''
         xtra = ''
         sflag = ''
